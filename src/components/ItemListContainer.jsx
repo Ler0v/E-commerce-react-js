5e0112478467f51319b2481727d8264e8f221f2c
Agustin <robledoagustin7@gmail.com>
2022-02-28 17:03:34 -0300

    components

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,9 +4,9 @@ import Productos from './Productos'
 const ItemListContainer = (props) => {
   return (
     <section className='row'>
-       <Productos titulo="Producto1" descripcion="este es un producto1" precio="99999" />
-       <Productos titulo="Producto2" descripcion="este es un producto2" precio="99999" />
-       <Productos titulo="Producto3" descripcion="este es un producto3" precio="99999" />
+       <Productos titulo="Producto1" descripcion="este es un producto1" comprar="añadir al carrito" />
+       <Productos titulo="Producto2" descripcion="este es un producto2" comprar="añadir al carrito" />
+       <Productos titulo="Producto3" descripcion="este es un producto3" comprar="añadir al carrito" />
     </section>
   )
 }
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Productos = (props) => {
-    let {titulo, descripcion, precio} = props
+    let {titulo, descripcion, comprar} = props
   return (
     <div className='col-lg-4 col-md-6 p-3'>
         <div className="card">
@@ -9,7 +9,7 @@ const Productos = (props) => {
           <div className="card-body">
             <h5 className="card-title">{titulo}</h5>
             <p className="card-text">{descripcion}</p>
-            <a href="#" className="btn btn-primary">{precio}</a>
+            <a href="#" className="btn btn-primary">{comprar}</a>
           </div>
         </div>
     </div>
