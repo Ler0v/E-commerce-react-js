6957c526f7eb9cb50aadb8026b263664954ffbeb
Ler0v <robledoagustin7@gmail.com>
2022-03-29 22:36:32 -0300

    Cart actualizado

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,13 +6,32 @@ import CartItem from './CartItem';
 const Cart = () => {
 
     const carritoContext = useContext(CartContext);
+    const productosCarrito = carritoContext.productosCarrito
     console.log('Productos Carrito', carritoContext.productosCarrito)
 
 
 
   return (
       <div>
-    
+           <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">SubTotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        productosCarrito.map(producto => (
+                        <CartItem key={producto.item.id} productoCarrito={producto}/>
+                        ))
+                    }
+                </tbody>
+                <button onClick={carritoContext.clear}>Vaciar Carrito</button>
+            </table>
       </div>
        
   )
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useContext } from 'react'
+import { CartContext } from '../Context/CartContext'
 
-const CartItem = () => {
+const CartItem = ({productoCarrito}) => {
+
+const carritoContext = useContext(CartContext);
+
   return (
-    <div>
-       hola
- </div>
-  )
+        <div style={{display:"flex", flexDirection:"column"}}>
+            <tr>
+                <td><img src={productoCarrito.item.imagen} style={{height:"200px"}} alt="Imagen Producto"/></td>
+                <td><p>{productoCarrito.item.nombre}</p></td>
+                <td><p>{productoCarrito.cantidad}</p></td>
+                <td><p>{productoCarrito.item.precio}</p></td>
+                <td><p>{productoCarrito.item.precio*productoCarrito.cantidad}</p></td>
+                <button onClick={()=>carritoContext.borrarItem(productoCarrito.item.id)}>Eliminar Producto</button>
+             </tr>
+        </div>
+    )
 }
 
 export default CartItem
