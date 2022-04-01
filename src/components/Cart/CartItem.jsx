b4de070f4315ced757aad6a718e0ce77fd478f12
Ler0v <robledoagustin7@gmail.com>
2022-03-31 23:27:09 -0300

    Carrito finalizado

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -7,16 +7,16 @@ const CartItem = ({productoCarrito}) => {
 const carritoContext = useContext(CartContext);
 
   return (
-        <div style={{display:"flex", flexDirection:"column"}}>
-            <tr>
-                <td><img src={productoCarrito.item.imagen} style={{height:"200px"}} alt="Imagen Producto"/></td>
-                <td><p>{productoCarrito.item.nombre}</p></td>
-                <td><p>{productoCarrito.cantidad}</p></td>
-                <td><p>{productoCarrito.item.precio}</p></td>
-                <td><p>{productoCarrito.item.precio*productoCarrito.cantidad}</p></td>
-                <button onClick={()=>carritoContext.borrarItem(productoCarrito.item.id)}>Eliminar Producto</button>
-             </tr>
-        </div>
+        <>
+        <tr>
+            <td><img src={productoCarrito.item.imagen} style={{height:"200px"}} alt="Imagen Producto"/></td>
+            <td><p>{productoCarrito.item.name}</p></td>
+            <td><p>{productoCarrito.cantidad}</p></td>
+            <td><p>{productoCarrito.item.price}</p></td>
+            <td><p>{productoCarrito.item.price*productoCarrito.cantidad}</p></td>
+            <button onClick={()=>carritoContext.borrarItem(productoCarrito.item.id)}>Eliminar Producto</button>
+            </tr>
+        </>
     )
 }
 
