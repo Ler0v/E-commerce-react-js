3bd832f50344d131406bbc8a31151ae4b104a171
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-04-28 01:55:26 -0300

    loader included

---
+++ src/components/Loading/Cargando.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cargando = () => {
+  return (
+    <>
+    <img src="https://i.postimg.cc/J72k8NWR/loading-icon-animated-gif-25.gif" alt="load" />
+    <p>Cargando...</p>
+    </>
+  )
+}
+
+export default Cargando
--- src/components/Product/ItemDetailContainer.jsx
+++ src/components/Product/ItemDetailContainer.jsx
@@ -2,15 +2,15 @@ import { doc,getDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import {db} from "../../utils/firebase"
-
 import ItemDetail from './ItemDetail'
+import Skeleton from 'react-loading-skeleton'
 
 
 
 const ItemDetailContainer = () => {
 
   const [item, setItem]= useState(null);
-
+  const [loading, setLoading]=useState(true)
   const {id} = useParams();
 
 
